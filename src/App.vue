f6c6225fea45eef8d56756aa981f73ccb1d9f528
Simone Fuchs <mail@simone-fuchs.de>
2024-03-23 11:19:04 +0100

    clean up markup and exchange logo

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,10 @@ const emailPlaceholder = 'stefan.schabernak@rockyourlife.de'
 
 const phone = ref('')
 const phonePlaceholder = '+49 (0) 89 / 224 106 22'
+
+const defaultFontStyle = "font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f;"
+const linkStyle = 'text-decoration: none; text-underline: none;'
+const highlightColor = 'color: #ec733b;'
 </script>
 
 <template>
@@ -32,95 +36,68 @@ const phonePlaceholder = '+49 (0) 89 / 224 106 22'
   </header>
 
   <main>
-    <p class="MsoNormal" style="margin-bottom: 8pt">
-      <span style="font-size: 10pt; font-family: 'Arial', sans-serif; color: black">
+    <p style="margin-bottom: 8pt">
+      <span :style="defaultFontStyle + 'font-size: 10pt; color: black'">
         {{ firstName || firstNamePlaceholder }} {{ lastName || lastNamePlaceholder }}
       </span>
       <br />
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f">
+      <span :style="defaultFontStyle">
         {{ position || positionPlaceholder }}
       </span>
     </p>
 
-    <p class="MsoNormal" style="margin-bottom: 8pt">
-      <img width="115" height="68" src="./assets/logo.png" v:shapes="_x0000_i1025" /><br />
-
-      <span
-        style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b; font-weight: bold"
-      >
+    <p style="margin-bottom: 8pt">
+      <img width="115" src="./assets/logo.jpeg" style="margin: 4pt 0" />
+      <br />
+      <span :style="defaultFontStyle + highlightColor + 'font-weight: bold'">
         ROCK YOUR LIFE! gGmbH
       </span>
       <br />
-      <span
-        style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f; mso-no-proof: yes"
-      >
-        Developing potentials since 2008.
-      </span>
+      <span :style="defaultFontStyle">Developing potentials since 2008.</span>
     </p>
 
-    <p class="MsoNormal" style="margin-bottom: 8pt">
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f">
-        Unsere Wirksamkeit ist wissenschaftlich bestätigt.
-      </span>
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b">
-        <a
-          href="https://rockyourlife.de/das-machen-wir/wirkung/"
-          style="text-decoration: none; text-underline: none"
-        >
-          <span style="color: #ec733b">Mehr erfahren</span>
-        </a>
+    <p style="margin-bottom: 8pt">
+      <span :style="defaultFontStyle">Unsere Wirksamkeit ist wissenschaftlich bestätigt.</span>
+      <a
+        :style="defaultFontStyle + linkStyle + highlightColor"
+        href="https://rockyourlife.de/das-machen-wir/wirkung/"
+      >
+        Mehr erfahren
+      </a>
+      <br />
+      <span :style="defaultFontStyle">Lust, uns zu unterstützen?</span>
+      <a
+        :style="defaultFontStyle + linkStyle + highlightColor"
+        href="https://rockyourlife.de/jetzt-spenden/?rock-your-life-ggmbh-3/spende"
+      >
+        Hier
+      </a>
+      <span :style="defaultFontStyle">können Sie für uns spenden.</span>
+    </p>
+
+    <p style="margin-bottom: 8pt">
+      <span :style="defaultFontStyle + highlightColor">Mail:&nbsp;&nbsp;&nbsp;</span>
+      <a :style="defaultFontStyle + linkStyle" :href="'mailto:' + email">
+        {{ email || emailPlaceholder }}
+      </a>
+      <br />
+      <span :style="defaultFontStyle + highlightColor">Mobil:&nbsp;&nbsp;</span>
+      <span :style="defaultFontStyle">{{ phone || phonePlaceholder }}</span>
+      <br />
+      <span :style="defaultFontStyle + highlightColor">Web:&nbsp;&nbsp;</span>
+      <a href="http://www.rockyourlife.de/" :style="defaultFontStyle + linkStyle">
+        www.rockyourlife.de
+      </a>
+    </p>
+
+    <p style="margin-bottom: 8pt">
+      <span :style="defaultFontStyle">
+        Sitz der Gesellschaft: München; Amtsgericht München HRB 212 029
       </span>
       <br />
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f">
-        Lust, uns zu unterstützen?&nbsp;
+      <span :style="defaultFontStyle">
+        Geschäftsführung: Elisabeth Hahnke, Marvin Hiltrop, Stefan Schabernak
       </span>
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b">
-        <a
-          href="https://rockyourlife.de/jetzt-spenden/?rock-your-life-ggmbh-3/spende"
-          style="text-decoration: none; text-underline: none"
-        >
-          <span style="color: #ec733b">Hier</span>
-        </a>
-      </span>
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f"
-        >&nbsp;können Sie für uns spenden.</span
-      >
-    </p>
-
-    <p class="MsoNormal" style="margin-bottom: 8pt">
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b">Mail:</span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f"
-        >&nbsp;&nbsp;&nbsp;<a
-          style="text-decoration: none; text-underline: none"
-          :href="'mailto:' + email"
-        >
-          <span style="color: #7f7f7f">{{ email || emailPlaceholder }}</span></a
-        ><br /> </span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b"
-        >Mobil&nbsp;&nbsp;</span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f">{{
-        phone || phonePlaceholder
-      }}</span
-      ><span style="font-size: 12pt; font-family: 'Times New Roman', serif; color: black"
-        ><br /> </span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #ec733b"
-        >Web:&nbsp;&nbsp;</span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f"
-        ><a href="http://www.rockyourlife.de/" style="text-decoration: none; text-underline: none"
-          ><span style="color: #7f7f7f; text-decoration: none; text-underline: none"
-            >www.rockyourlife.de</span
-          ></a
-        >
-      </span>
-    </p>
-
-    <p class="MsoNormal" style="margin-bottom: 8pt">
-      <span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: #7f7f7f"
-        >Sitz der Gesellschaft:&nbsp;</span
-      ><span style="font-size: 8pt; font-family: 'Arial', sans-serif; color: gray"
-        >München; Amtsgericht München HRB 212 029<br />
-        Geschäftsführung: Elisabeth Hahnke, Marvin Hiltrop, Stefan Schabernak</span
-      >
     </p>
   </main>
 </template>
